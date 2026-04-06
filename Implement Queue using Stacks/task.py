a5c7d90b2d54837212f44b7efdd06e95bc200a0a
Ustymer <trukhin.pn@ucu.edu.ua>
2026-04-06 13:46:45 +0300

    Update task.py

--- Implement Queue using Stacks/task.py
+++ Implement Queue using Stacks/task.py
@@ -1,33 +1,44 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+    def push(self, val):
+        node = Node(val)
+        node.next = self.top
+        self.top = node
+    def pop(self):
+        val = self.top.val
+        self.top = self.top.next
+        return val
+
+    def peek(self):
+        return self.top.val
+
+    def is_empty(self):
+        return self.top is None
 class MyQueue:
     def __init__(self):
-        self.stack_in = []
-        self.stack_out = []
-        
+        self.stack_in = Stack()
+        self.stack_out = Stack()
 
     def push(self, x: int) -> None:
-        self.stack_in.append(x)
-
+        self.stack_in.push(x)
     def pop(self) -> int:
-        if not self.stack_out:
-            while self.stack_in:
-                self.stack_out.append(self.stack_in.pop())
+        if self.stack_out.is_empty():
+            while not self.stack_in.is_empty():
+                self.stack_out.push(self.stack_in.pop())
         return self.stack_out.pop()
 
+
     def peek(self) -> int:
-        if not self.stack_out:
-            while self.stack_in:
-                self.stack_out.append(self.stack_in.pop())
-        return self.stack_out[-1]
-        
+        if self.stack_out.is_empty():
+            while not self.stack_in.is_empty():
+                self.stack_out.push(self.stack_in.pop())
+        return self.stack_out.peek()
 
     def empty(self) -> bool:
-        return not self.stack_in and not self.stack_out
-        
-
-
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+        return self.stack_in.is_empty() and self.stack_out.is_empty()
